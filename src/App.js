610bb8c1c7c0564c9f3328c6b85614c8c6b18707
Alex Martrain <alexander.martrain.2020@stpauls.com>
2023-06-06 12:21:28 -0500

    added placeholder resume page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import Header from './components/Header/Header'
 import About from './components/About/About'
 import Projects from './components/Projects/Projects'
 import ContactMe from './components/ContactMe/ContactMe'
+import Resume from './components/Resume/Resume'
 
 function App() {
   return (
     <>
     <Header />
-    <ContactMe />
+    <Resume />
     </>
   );
 }
---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './resumeStyle.css'
+
+const Resume = () => {
+  return (
+    <div className='resumeContainer'>
+        <h1 className='placeholderText'>I have not written a professional resume yet, I am awaiting a response from career services. It will be inputted with a pdf download as soon as I have made one I am confident in.</h1>
+    </div>
+  )
+}
+
+export default Resume
